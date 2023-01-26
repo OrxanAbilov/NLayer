1c626aede1a9c94f0fc22bb71812046a8ac8da78
orxanabilov <abilov_orxan@hotmail.com>
2023-01-27 00:16:00 +0400

    GenericRepository impl

---
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(
+                new ProductFeature { Id = 1, ProductId = 1, Color = "Black" },
+                new ProductFeature { Id = 2, ProductId = 2, Color = "White" },
+                new ProductFeature { Id = 3, ProductId = 3, Color = "Red" }
+                );
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, CategoryId = 1, Name = "Samsung Galaxy A32", Price = 560, Stock = 30 },
+                new Product { Id = 2, CategoryId = 1, Name = "Honor X9", Price = 600, Stock = 20 },
+                new Product { Id = 3, CategoryId = 2, Name = "IPad", Price = 900, Stock = 10 },
+                new Product { Id = 4, CategoryId = 2, Name = "Samsung Tab", Price = 400, Stock = 20 },
+                new Product { Id = 5, CategoryId = 3, Name = "Hp Pavilion 15", Price = 750, Stock = 10 },
+                new Product { Id = 5, CategoryId = 3, Name = "Lenovo ThinkPad", Price = 1500, Stock = 10 }
+                );
+        }
+    }
+}
